f06ad3d235f322916bfc0b6824a0e407f74d9768
Neil Martin <neil.martin83@gmail.com>
2019-09-20 05:38:00 +0100

    Added sleep to let things settle down before loading and tweaked to avoid having to kill the login window later on.

--- example_pkg_postinstall.sh
+++ example_pkg_postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-/usr/local/bin/authchanger -reset -preLogin NoMADLoginAD:Notify
-/bin/echo "Command: Image: "/Library/Application Support/UEL/branding/logo.png"" >> /var/tmp/depnotify.log
+# 2019-09-20 Added sleep to let things settle down before loading and tweaked to avoid having to kill the login window later on.
+/bin/echo "Command: Image: "/Library/Application Support/UEL/branding/logo-prov.png"" >> /var/tmp/depnotify.log
 /bin/echo "Command: MainTitle: Please wait a moment..."  >> /var/tmp/depnotify.log
 /bin/echo "Command: MainText: " >> /var/tmp/depnotify.log
 /bin/echo "Status: Please wait..." >> /var/tmp/depnotify.log
+/usr/local/bin/authchanger -reset -preLogin NoMADLoginAD:Notify NoMADLoginAD:UserInput NoMADLoginAD:Notify
+/bin/sleep 10
